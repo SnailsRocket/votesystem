05250fff92befeb1117401faf51909503e7e1a5c
xubo <ABCabc123>
2022-02-24 17:57:09 +0800

    查询接口编写

--- src/main/java/com/xubo/votesystem/Application.java
+++ src/main/java/com/xubo/votesystem/Application.java
@@ -1,5 +1,6 @@
 package com.xubo.votesystem;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Author xubo
  * @Date 2022/2/24 9:44
  * 投票系统
+ * 实体类之间的关系紊乱 后期优化
  */
 @SpringBootApplication
+@MapperScan(basePackages = "com.xubo.votesystem.dao")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/main/java/com/xubo/votesystem/dao/VoteContentMapper.java
+++ src/main/java/com/xubo/votesystem/dao/VoteContentMapper.java
@@ -3,11 +3,12 @@ package com.xubo.votesystem.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.xubo.votesystem.entitys.domain.VoteContent;
+import com.xubo.votesystem.entitys.vo.VoteInfoVO;
 
 /**
 * @Entity generator.domain.VoteContent
 */
 public interface VoteContentMapper extends BaseMapper<VoteContent> {
 
-
+    VoteInfoVO getDetailById(Integer voteId);
 }
