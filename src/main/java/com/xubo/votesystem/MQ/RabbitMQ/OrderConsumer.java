f2367ff20aa6af3ec35052775a9f9e70f84b4b9f
xubo <ABCabc123>
2022-04-02 13:16:45 +0800

    RabbitMQ消息队列

--- src/main/java/com/xubo/votesystem/MQ/RabbitMQ/OrderConsumer.java
+++ src/main/java/com/xubo/votesystem/MQ/RabbitMQ/OrderConsumer.java
@@ -10,6 +10,9 @@ import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @Author xubo
  * @Date 2022/3/29 11:18
@@ -31,6 +34,20 @@ public class OrderConsumer {
         }
         log.info("消费数据：" +receive);
     }
+
+    public static void main(String[] args) {
+
+        List<Integer> i2 = Arrays.asList(3,5,6);
+        List<Integer> i1= Arrays.asList(1,2,3,4);
+
+        System.out.println(i2.containsAll(i1));
+        Integer integer = Integer.valueOf(2);
+        Integer integer1 = Integer.valueOf(1);
+        Integer integer2 = Integer.valueOf(2);
+        System.out.println(integer.compareTo(integer2));
+
+    }
+
 
 
 }
